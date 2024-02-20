d976fb6fc753a733b12792e3630cec925f00fde2
itaispiegel <itai.spiegel@gmail.com>
2024-02-20 23:31:46 +0200

    Start implementing HTTP proxy

--- user/main.go
+++ user/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "github.com/itaispiegel/infosec-workshop/user/cmd"
+import (
+	"os"
+
+	"github.com/itaispiegel/infosec-workshop/user/cmd"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
 
 func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	cmd.Execute()
 }
