1e5d5ba75d8e19ff27c81fc9bf2232cf0c9b10fe
itaispiegel <itai.spiegel@gmail.com>
2024-02-23 18:10:33 +0200

    Add docs to proxy functions and define macros with the proxy IP addresses

--- module/proxy.h
+++ module/proxy.h
@@ -5,7 +5,27 @@
 
 #include "parser.h"
 
+#define FW_INTERNAL_PROXY_IP 0x0301010a
+#define FW_EXTERNAL_PROXY_IP 0x0302010a
+
+/**
+ * Receives a TCP packet received by the PRE_ROUTING hook.
+ * If the packet is an HTTP or FTP packet sent from a client in the internal
+ * network, to a server in the external network, the destination IP and port
+ * will be changed to the proxy process running in the userspace. The SKB's
+ * addresses will change to the new addresses, but the packet struct will not.
+ * The IPv4 and TCP checksums are guaranteed to be fixed by the function.
+ */
 void proxy_client_request(packet_t *packet, struct sk_buff *skb);
+
+/**
+ * Receives a TCP packet received by the LOCAL_OUT hook.
+ * If the packet is from one of the userspace proxy processes to a client in the
+ * internal network, the source address and port will be changed to the address
+ * and port of the server in the external network that sent the packet.
+ * Both the SKB and packet structs will be changed, and the IPv4 and TCP
+ * checksums are guaranteed to be fixed.
+ */
 void proxy_server_response(packet_t *packet, struct sk_buff *skb);
 
 #endif // _PROXY_H
