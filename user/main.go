cebe5d86dcbda90ebe528db4aeafcf0bcf8acc07
itaispiegel <itai.spiegel@gmail.com>
2024-02-24 17:08:16 +0200

    Extract the main logic of the proxy to a proxy struct

--- user/main.go
+++ user/main.go
@@ -11,32 +11,4 @@ import (
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	cmd.Execute()
-	// clientSock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, syscall.IPPROTO_TCP)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// if err := syscall.Bind(clientSock, &syscall.SockaddrInet4{Port: 0, Addr: [4]byte{127, 0, 0, 1}}); err != nil {
-	// 	panic(err)
-	// }
-
-	// sockAddr, err := syscall.Getsockname(clientSock)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// port := sockAddr.(*syscall.SockaddrInet4).Port
-	// fmt.Println("Connecting via port: ", port)
-
-	// if err := syscall.Connect(clientSock, &syscall.SockaddrInet4{Port: 12345, Addr: [4]byte{127, 0, 0, 1}}); err != nil {
-	// 	panic(err)
-	// }
-
-	// conn, err := net.FileConn(os.NewFile(uintptr(clientSock), "socket"))
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// conn.Write([]byte("Hello, world!"))
-	// conn.Close()
 }
--- user/pkg/proxy/proxy.go
+++ user/pkg/proxy/proxy.go
@@ -4,37 +4,162 @@ import (
 	"bytes"
 	"encoding/binary"
 	"errors"
+	"fmt"
+	"io"
 	"net"
 	"os"
-	"strconv"
-	"strings"
+	"syscall"
 
 	"github.com/itaispiegel/infosec-workshop/user/pkg/conntrack"
 	"github.com/itaispiegel/infosec-workshop/user/pkg/utils"
+	"github.com/rs/zerolog/log"
 )
 
 const (
 	setProxyPortFile = "/sys/class/fw/proxy_port/proxy_port"
 )
 
-type Proxy interface {
-	Start() error
-	handleConnection() error
+// DataCallback is a function that is called when data is received,
+// and returns a boolean reperesenting whether to pass the data on
+type DataCallback func(data []byte) bool
+
+type Proxy struct {
+	Address string
+	Port    uint16
+	DataCallback
 }
 
-func lookupPeerAddr(addr net.Addr) (*net.TCPAddr, error) {
+func (p *Proxy) Start() error {
+	bindAddr := fmt.Sprintf("%s:%d", p.Address, p.Port)
+	log.Info().Msgf("Starting HTTP proxy server on %s", bindAddr)
+	proxyListener, err := net.Listen("tcp4", bindAddr)
+	if err != nil {
+		return err
+	}
+	defer proxyListener.Close()
+
+	for {
+		clientConn, err := proxyListener.Accept()
+		if err != nil {
+			return err
+		}
+
+		go p.handleConnection(clientConn)
+	}
+}
+
+func (p *Proxy) handleConnection(proxyToClientConn net.Conn) {
+	defer proxyToClientConn.Close()
+	clientAddr := proxyToClientConn.RemoteAddr().(*net.TCPAddr)
+	log.Debug().Msgf("Accepted connection from %s. "+
+		"Looking up server address from the connections table", clientAddr)
+	serverAddr, err := lookupPeerAddr(clientAddr)
+	if err != nil {
+		log.Error().Err(err).Msg("Error looking up server address in the connections table")
+		return
+	}
+
+	proxyToServerConn, err := connectToServer(clientAddr, serverAddr)
+	if err != nil {
+		log.Error().Err(err).Msg("Error connecting to server")
+		return
+	}
+	defer proxyToServerConn.Close()
+
+	log.Info().
+		Str("clientAddr", clientAddr.String()).
+		Str("serverAddr", serverAddr.String()).
+		Str("proxyAddr", proxyToServerConn.LocalAddr().String()).
+		Msg("Forwarding session to server")
+
+	done := make(chan struct{})
+	go p.forwardConnections(proxyToServerConn, proxyToClientConn, done)
+	go p.forwardConnections(proxyToClientConn, proxyToServerConn, done)
+
+	<-done
+	<-done
+
+	log.Info().
+		Str("clientAddr", clientAddr.String()).
+		Str("serverAddr", serverAddr.String()).
+		Str("proxyAddr", proxyToServerConn.LocalAddr().String()).
+		Msg("Closed forwarding connection")
+}
+
+func (p *Proxy) forwardConnections(source, dest net.Conn, done chan struct{}) {
+	buffer := make([]byte, 1024)
+	for {
+		n, err := source.Read(buffer)
+		if errors.Is(err, io.EOF) {
+			dest.Close()
+			done <- struct{}{}
+			return
+		} else if errors.Is(err, net.ErrClosed) {
+			done <- struct{}{}
+			return
+		} else if err != nil {
+			log.Error().Err(err).Msg("Error reading from server")
+			dest.Close()
+			done <- struct{}{}
+			return
+		} else {
+			if p.DataCallback(buffer[:n]) {
+				dest.Write(buffer[:n])
+			} else {
+				log.Warn().Msg("Packet didn't pass callback, closing connection")
+				dest.Close()
+				done <- struct{}{}
+				return
+			}
+		}
+	}
+}
+
+func connectToServer(clientAddr *net.TCPAddr, serverAddr *net.TCPAddr) (net.Conn, error) {
+	proxyToServerSock, err := syscall.Socket(syscall.AF_INET, syscall.SOCK_STREAM, syscall.IPPROTO_TCP)
+	if err != nil {
+		return nil, err
+	}
+
+	defer syscall.Close(proxyToServerSock)
+
+	if err := syscall.Bind(proxyToServerSock, &syscall.SockaddrInet4{}); err != nil {
+		return nil, err
+	}
+
+	sockAddr, err := syscall.Getsockname(proxyToServerSock)
+	if err != nil {
+		return nil, err
+	}
+
+	sockAddrInet4, ok := sockAddr.(*syscall.SockaddrInet4)
+	if !ok {
+		return nil, errors.New("unexpected socket address type")
+	}
+
+	proxyPort := uint16(sockAddrInet4.Port)
+	setProxyPort(clientAddr, serverAddr, proxyPort)
+
+	serverAddrInet4 := &syscall.SockaddrInet4{Port: serverAddr.Port, Addr: [4]byte(serverAddr.IP.To4())}
+	if err := syscall.Connect(proxyToServerSock, serverAddrInet4); err != nil {
+		return nil, err
+	}
+
+	proxyToServerConn, err := net.FileConn(os.NewFile(uintptr(proxyToServerSock), ""))
+	return proxyToServerConn, err
+}
+
+func lookupPeerAddr(addr *net.TCPAddr) (*net.TCPAddr, error) {
 	connections, err := conntrack.ReadConnections()
 	if err != nil {
 		return nil, err
 	}
 
-	addrParts := strings.Split(addr.String(), ":")
-	addrIp := [4]byte(net.ParseIP(addrParts[0]).To4())
-	addrPortInt, _ := strconv.Atoi(addrParts[1])
-	addrPort := uint16(addrPortInt)
+	addrIp := [4]byte(addr.IP.To4())
+	addrPort := uint16(addr.Port)
 
 	for _, conn := range connections {
-		if conn.SrcIp == addrIp && conn.SrcPort == uint16(addrPort) {
+		if conn.SrcIp == addrIp && conn.SrcPort == addrPort {
 			return &net.TCPAddr{
 				IP:   conn.DestIp[:],
 				Port: int(conn.DestPort),
@@ -45,22 +170,12 @@ func lookupPeerAddr(addr net.Addr) (*net.TCPAddr, error) {
 	return nil, errors.New("no matching connection found")
 }
 
-func setProxyPort(clientAddr, serverAddr net.Addr, proxyPort uint16) {
-	clientAddrParts := strings.Split(clientAddr.String(), ":")
-	clientAddrIp := [4]byte(net.ParseIP(clientAddrParts[0]).To4())
-	clientAddrPortInt, _ := strconv.Atoi(clientAddrParts[1])
-	clientAddrPort := uint16(clientAddrPortInt)
-
-	serverAddrParts := strings.Split(serverAddr.String(), ":")
-	serverAddrIp := [4]byte(net.ParseIP(serverAddrParts[0]).To4())
-	serverAddrPortInt, _ := strconv.Atoi(serverAddrParts[1])
-	serverAddrPort := uint16(serverAddrPortInt)
-
+func setProxyPort(clientAddr, serverAddr *net.TCPAddr, proxyPort uint16) {
 	buf := bytes.NewBuffer(nil)
-	utils.PanicIfError(binary.Write(buf, binary.BigEndian, clientAddrIp))
-	utils.PanicIfError(binary.Write(buf, binary.BigEndian, clientAddrPort))
-	utils.PanicIfError(binary.Write(buf, binary.BigEndian, serverAddrIp))
-	utils.PanicIfError(binary.Write(buf, binary.BigEndian, serverAddrPort))
+	utils.PanicIfError(binary.Write(buf, binary.BigEndian, [4]byte(clientAddr.IP.To4())))
+	utils.PanicIfError(binary.Write(buf, binary.BigEndian, uint16(clientAddr.Port)))
+	utils.PanicIfError(binary.Write(buf, binary.BigEndian, [4]byte(serverAddr.IP.To4())))
+	utils.PanicIfError(binary.Write(buf, binary.BigEndian, uint16(serverAddr.Port)))
 	utils.PanicIfError(binary.Write(buf, binary.BigEndian, proxyPort))
 
 	if err := os.WriteFile(setProxyPortFile, buf.Bytes(), 0); err != nil {
