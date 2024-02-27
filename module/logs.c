456bae877e2dcecb30fc4a610cf6c4d67e8b7aea
itaispiegel <itai.spiegel@gmail.com>
2024-02-27 10:58:36 +0200

    Extract log functions to logs.h

--- module/logs.c
+++ module/logs.c
@@ -6,8 +6,8 @@
 
 #include "logs.h"
 
-LIST_HEAD(logs_list);
-size_t logs_count = 0;
+static LIST_HEAD(logs_list);
+static size_t logs_count = 0;
 
 static int show_logs_dev_major;
 static struct device *show_logs_dev;
@@ -15,6 +15,13 @@ static struct device *show_logs_dev;
 static int reset_logs_dev_major;
 static struct device *reset_logs_dev;
 
+/**
+ * Read the logs from the logs list and write them to the character device.
+ * The device supports reading from arbitrary offsets, but they must be a
+ * multiple of the log row size.
+ * If the offset is greater than the number of logs in the list, or if the read
+ * size is less than a single log row, the function will return zero bytes.
+ */
 ssize_t show_logs_dev_read(struct file *filp, char *buf, size_t len,
                            loff_t *off);
 
@@ -44,6 +51,65 @@ static ssize_t reset_logs_store(struct device *dev,
 }
 
 static DEVICE_ATTR(reset, S_IWUSR, NULL, reset_logs_store);
+
+static inline bool log_match_rule(log_row_t *log_row, rule_t *rule) {
+    return log_row->protocol == rule->protocol &&
+           log_row->action == rule->action && log_row->src_ip == rule->src_ip &&
+           log_row->dst_ip == rule->dst_ip &&
+           log_row->src_port == rule->src_port &&
+           log_row->dst_port == rule->dst_port;
+}
+
+static inline log_row_t new_log_row_by_packet(packet_t *packet, reason_t reason,
+                                              __u8 verdict) {
+    return (log_row_t){
+        .timestamp = ktime_get_real_seconds(),
+        .protocol = packet->protocol,
+        .action = verdict,
+        .src_ip = packet->src_ip,
+        .dst_ip = packet->dst_ip,
+        .src_port = packet->src_port,
+        .dst_port = packet->dst_port,
+        .reason = reason,
+        .count = 1,
+    };
+}
+
+static inline bool log_entry_matches_packet(struct log_entry *log_entry,
+                                            packet_t *packet) {
+    return log_entry->log_row.protocol == packet->protocol &&
+           log_entry->log_row.src_ip == packet->src_ip &&
+           log_entry->log_row.dst_ip == packet->dst_ip &&
+           log_entry->log_row.src_port == packet->src_port &&
+           log_entry->log_row.dst_port == packet->dst_port;
+}
+
+void update_log_entry_by_packet(packet_t *packet, reason_t reason,
+                                __u8 verdict) {
+    struct log_entry *log_entry;
+    struct list_head *pos;
+    list_for_each(pos, &logs_list) {
+        log_entry = list_entry(pos, struct log_entry, list);
+        if (log_entry->log_row.reason == reason &&
+            log_entry_matches_packet(log_entry, packet)) {
+            log_entry->log_row.count++;
+            log_entry->log_row.timestamp = ktime_get_real_seconds();
+            return;
+        }
+    }
+
+    printk(KERN_INFO "Creating a new log entry for packet\n");
+    log_entry = kmalloc(sizeof(struct log_entry), GFP_KERNEL);
+    if (log_entry == NULL) {
+        printk(KERN_WARNING
+               "Failed to allocate memory for log entry, so ignoring it\n");
+        return;
+    }
+
+    log_entry->log_row = new_log_row_by_packet(packet, reason, verdict);
+    list_add_tail(&log_entry->list, &logs_list);
+    logs_count++;
+}
 
 /**
  * Read the logs from the logs list and write them to the character device.
--- module/netfilter_hook.c
+++ module/netfilter_hook.c
@@ -11,9 +11,6 @@
 #include "tcp_conntrack.h"
 #include "types.h"
 
-extern struct list_head logs_list;
-extern size_t logs_count;
-
 extern rule_t rules[MAX_RULES];
 extern __u8 rules_count;
 
@@ -33,65 +30,6 @@ static const struct nf_hook_ops nf_local_out_op = {
     .hooknum = NF_INET_LOCAL_OUT,
     .priority = NF_IP_PRI_FIRST,
 };
-
-static inline bool log_match_rule(log_row_t *log_row, rule_t *rule) {
-    return log_row->protocol == rule->protocol &&
-           log_row->action == rule->action && log_row->src_ip == rule->src_ip &&
-           log_row->dst_ip == rule->dst_ip &&
-           log_row->src_port == rule->src_port &&
-           log_row->dst_port == rule->dst_port;
-}
-
-static inline log_row_t new_log_row_by_packet(packet_t *packet, reason_t reason,
-                                              __u8 verdict) {
-    return (log_row_t){
-        .timestamp = ktime_get_real_seconds(),
-        .protocol = packet->protocol,
-        .action = verdict,
-        .src_ip = packet->src_ip,
-        .dst_ip = packet->dst_ip,
-        .src_port = packet->src_port,
-        .dst_port = packet->dst_port,
-        .reason = reason,
-        .count = 1,
-    };
-}
-
-static inline bool log_entry_matches_packet(struct log_entry *log_entry,
-                                            packet_t *packet) {
-    return log_entry->log_row.protocol == packet->protocol &&
-           log_entry->log_row.src_ip == packet->src_ip &&
-           log_entry->log_row.dst_ip == packet->dst_ip &&
-           log_entry->log_row.src_port == packet->src_port &&
-           log_entry->log_row.dst_port == packet->dst_port;
-}
-
-static void update_log_entry_by_packet(packet_t *packet, reason_t reason,
-                                       __u8 verdict) {
-    struct log_entry *log_entry;
-    struct list_head *pos;
-    list_for_each(pos, &logs_list) {
-        log_entry = list_entry(pos, struct log_entry, list);
-        if (log_entry->log_row.reason == reason &&
-            log_entry_matches_packet(log_entry, packet)) {
-            log_entry->log_row.count++;
-            log_entry->log_row.timestamp = ktime_get_real_seconds();
-            return;
-        }
-    }
-
-    printk(KERN_INFO "Creating a new log entry for packet\n");
-    log_entry = kmalloc(sizeof(struct log_entry), GFP_KERNEL);
-    if (log_entry == NULL) {
-        printk(KERN_WARNING
-               "Failed to allocate memory for log entry, so ignoring it\n");
-        return;
-    }
-
-    log_entry->log_row = new_log_row_by_packet(packet, reason, verdict);
-    list_add_tail(&log_entry->list, &logs_list);
-    logs_count++;
-}
 
 static unsigned int netfilter_hook_func(void *priv, struct sk_buff *skb,
                                         const struct nf_hook_state *state) {
