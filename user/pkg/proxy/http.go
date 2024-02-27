026fec37adf987d7cb885b2163ed9ca284e5eebc
itaispiegel <itai.spiegel@gmail.com>
2024-02-27 13:57:40 +0200

    Fixed an error that caused the connections between the proxy and the server to keepalive

--- user/pkg/proxy/http.go
+++ user/pkg/proxy/http.go
@@ -7,32 +7,33 @@ import (
 	"strings"
 )
 
-func rejectCsvFiles(data []byte) bool {
-	return !strings.Contains(string(data), "Content-Type: text/csv")
-}
+func blockCsvCallback(data []byte, dest net.Conn) bool {
+	if !strings.Contains(string(data), "Content-Type: text/csv") {
+		dest.Write(data)
+		return false
+	} else {
+		msg := "Blocked by Firewall\n"
+		readerCloser := io.NopCloser(strings.NewReader(msg))
+		resp := http.Response{
+			Status:     "403 Forbidden",
+			StatusCode: 403,
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+			Header: map[string][]string{
+				"Content-Type": {"text/plain"},
+			},
+			Body: readerCloser,
+		}
 
-func respondBlockedByFirewall(conn net.Conn) {
-	msg := "Blocked by Firewall\n"
-	readerCloser := io.NopCloser(strings.NewReader(msg))
-	resp := http.Response{
-		Status:     "403 Forbidden",
-		StatusCode: 403,
-		ProtoMajor: 1,
-		ProtoMinor: 1,
-		Header: map[string][]string{
-			"Content-Type": {"text/plain"},
-		},
-		Body: readerCloser,
+		resp.Write(dest)
+		return true
 	}
-
-	resp.Write(conn)
 }
 
 func NewHttpProxy(address string, port uint16) *Proxy {
 	return &Proxy{
-		Address:                   address,
-		Port:                      port,
-		PacketFilter:              rejectCsvFiles,
-		OnSessionRejectedCallback: respondBlockedByFirewall,
+		Address:        address,
+		Port:           port,
+		PacketCallback: blockCsvCallback,
 	}
 }
