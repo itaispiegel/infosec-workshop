bf977b0229bdb260a98968c85550f89b6685638f
itaispiegel <itai.spiegel@gmail.com>
2024-03-01 14:50:39 +0200

    Block also ZIP files in HTTP connections

--- user/pkg/proxy/http.go
+++ user/pkg/proxy/http.go
@@ -4,35 +4,48 @@ import (
 	"io"
 	"net"
 	"net/http"
+	"regexp"
 	"strings"
 
 	"github.com/rs/zerolog"
 )
 
-func blockCsvCallback(data []byte, dest net.Conn, logger zerolog.Logger) bool {
-	var isValid bool
-	if isValid = !strings.Contains(string(data), "Content-Type: text/csv"); !isValid {
-		dest.Write(data)
-	} else {
-		msg := "Blocked by Firewall\n"
-		readerCloser := io.NopCloser(strings.NewReader(msg))
-		resp := http.Response{
-			Status:     "403 Forbidden",
-			StatusCode: 403,
-			ProtoMajor: 1,
-			ProtoMinor: 1,
-			Header: map[string][]string{
-				"Content-Type": {"text/plain"},
-			},
-			Body: readerCloser,
-		}
+var dangerousContentTypes = []string{
+	"text/csv",
+	"application/zip",
+}
 
-		resp.Write(dest)
-		logger.Warn().Str("srcAddr", dest.LocalAddr().String()).
-			Str("destAddr", dest.RemoteAddr().String()).
-			Msg("Blocked CSV file")
+func sendBlockedResponse(dest net.Conn) {
+	resp := http.Response{
+		Status:     "403 Forbidden",
+		StatusCode: 403,
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Header: map[string][]string{
+			"Content-Type": {"text/plain"},
+		},
+		Body: io.NopCloser(strings.NewReader("Blocked by Firewall\n")),
 	}
-	return isValid
+
+	resp.Write(dest)
+}
+
+func blockDangerousFilesCallback(data []byte, dest net.Conn, logger zerolog.Logger) bool {
+	matches := regexp.MustCompile("Content-Type: (.*)\r\n").FindSubmatch(data)
+	if len(matches) > 1 {
+		contentType := string(matches[1])
+		for i := range dangerousContentTypes {
+			if contentType == dangerousContentTypes[i] {
+				sendBlockedResponse(dest)
+				logger.Warn().Str("srcAddr", dest.LocalAddr().String()).
+					Str("destAddr", dest.RemoteAddr().String()).
+					Msg("Blocked CSV file")
+				return false
+			}
+		}
+	}
+	dest.Write(data)
+	return true
 }
 
 func NewHttpProxy(address string, port uint16) *Proxy {
@@ -40,6 +53,6 @@ func NewHttpProxy(address string, port uint16) *Proxy {
 		Protocol:       "http",
 		Address:        address,
 		Port:           port,
-		PacketCallback: blockCsvCallback,
+		PacketCallback: blockDangerousFilesCallback,
 	}
 }
