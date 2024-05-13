fe1c23a3be079fd3d08119c868b19b1b52de921a
itaispiegel <itai.spiegel@gmail.com>
2024-05-13 14:17:50 +0300

    Split cparser tests to different files

--- user/cparser/preprocessor_test.go
+++ user/cparser/preprocessor_test.go
@@ -6,19 +6,14 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"gopkg.in/yaml.v3"
-)
-
-const (
-	testDataDir = "testdata"
 )
 
 func TestPreprocessor(t *testing.T) {
 	testCases := getTestCases(t)
 	for _, testName := range testCases {
 		t.Run(testName, func(t *testing.T) {
-			inputFilePath := path.Join(testDataDir, testName, "input.c")
-			preprocessedFilePath := path.Join(testDataDir, testName, "preprocessed.c")
+			inputFilePath := path.Join(testDataDir, testName, testInputFileName)
+			preprocessedFilePath := path.Join(testDataDir, testName, testPreprocessedFileName)
 
 			input, err := os.ReadFile(inputFilePath)
 			assert.NoError(t, err)
@@ -33,37 +28,4 @@ func TestPreprocessor(t *testing.T) {
 			}
 		})
 	}
-}
-
-func TestParser(t *testing.T) {
-	testCases := getTestCases(t)
-	for _, testName := range testCases {
-		t.Run(testName, func(t *testing.T) {
-			inputFilePath := path.Join(testDataDir, testName, "input.c")
-			parserStatusPath := path.Join(testDataDir, testName, "parser.txt")
-
-			input, err := os.ReadFile(inputFilePath)
-			assert.NoError(t, err)
-
-			expectedParserStatus := CParserResult{}
-			parserStatusBytes, err := os.ReadFile(parserStatusPath)
-			assert.NoError(t, err)
-			yaml.Unmarshal(parserStatusBytes, &expectedParserStatus)
-
-			actualParserStatus := Parse(string(input))
-			assert.Equal(t, expectedParserStatus, actualParserStatus, "Test case '%s' failed", testName)
-		})
-	}
-}
-
-func getTestCases(t *testing.T) []string {
-	entries, err := os.ReadDir(testDataDir)
-	assert.NoError(t, err)
-	testCases := make([]string, 0)
-	for _, entry := range entries {
-		if entry.IsDir() {
-			testCases = append(testCases, entry.Name())
-		}
-	}
-	return testCases
 }
